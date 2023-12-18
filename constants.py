08af9acc0a120344352cb7eca603ba1a575b8a18
Natan Almeida <natan.almeida03@gmail.com>
2023-12-17 21:26:04 -0300

    atualizado com os novos modelos de personagem e novas animacoes

--- constants.py
+++ constants.py
@@ -9,7 +9,7 @@ HEIGHT = 500
 TITLE = 'LEGENDS BRAZILIAN'
 
 #FPS
-FPS = 60
+FPS = 30
 
 #Cores
 BLACK = (0, 0, 0)
@@ -21,6 +21,56 @@ RED = (226, 10, 16)
 #Imagens
 LOGO = pygame.transform.scale(pygame.image.load(os.path.join('img', 'logo.png')), (80, 80))
 IMAGE_CUCA = pygame.transform.scale(pygame.image.load(os.path.join('img', 'cuca.png')), (100, 100))
+CUCA_SPRITE = [pygame.transform.scale(pygame.image.load("./img/cuca/cuca-1.png"), (200,200)),
+               pygame.transform.scale(pygame.image.load("./img/cuca/cuca-2.png"),(200, 200)),
+               pygame.transform.scale(pygame.image.load("./img/cuca/cuca-3.png") ,(200, 200)),
+               pygame.transform.scale(pygame.image.load("./img/cuca/cuca-4.png") ,(200, 200)),
+               pygame.transform.scale(pygame.image.load("./img/cuca/cuca-5.png") ,(200, 200)),
+               pygame.transform.scale(pygame.image.load("./img/cuca/cuca-6.png") ,(200, 200)),
+               pygame.transform.scale(pygame.image.load("./img/cuca/cuca-7.png") ,(200, 200)),
+               ]
+SACI_SPRITE = [pygame.transform.scale(pygame.image.load("./img/saci/sacy-1.png"), (300,300)),
+               pygame.transform.scale(pygame.image.load("./img/saci/sacy-2.png"),(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/saci/sacy-3.png") ,(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/saci/sacy-4.png") ,(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/saci/sacy-5.png") ,(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/saci/sacy-6.png") ,(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/saci/sacy-7.png") ,(300, 300)),
+               ]
+MULA_SPRITE = [pygame.transform.scale(pygame.image.load("./img/mula/mula-1.png"), (300,300)),
+               pygame.transform.scale(pygame.image.load("./img/mula/mula-2.png"),(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/mula/mula-3.png") ,(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/mula/mula-4.png") ,(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/mula/mula-5.png") ,(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/mula/mula-6.png") ,(300, 300)),
+               pygame.transform.scale(pygame.image.load("./img/mula/mula-7.png") ,(300, 300)),
+               ]
+
+CUCA_SPRITE_INV = [pygame.transform.flip(CUCA_SPRITE[0], True, False),
+                   pygame.transform.flip(CUCA_SPRITE[1], True, False),
+                   pygame.transform.flip(CUCA_SPRITE[2], True, False),
+                   pygame.transform.flip(CUCA_SPRITE[3], True, False),
+                   pygame.transform.flip(CUCA_SPRITE[4], True, False),
+                   pygame.transform.flip(CUCA_SPRITE[5], True, False),
+                   pygame.transform.flip(CUCA_SPRITE[6], True, False),
+                   ]
+SACI_SPRITE_INV = [pygame.transform.flip(SACI_SPRITE[0], True, False),
+                   pygame.transform.flip(SACI_SPRITE[1], True, False),
+                   pygame.transform.flip(SACI_SPRITE[2], True, False),
+                   pygame.transform.flip(SACI_SPRITE[3], True, False),
+                   pygame.transform.flip(SACI_SPRITE[4], True, False),
+                   pygame.transform.flip(SACI_SPRITE[5], True, False),
+                   pygame.transform.flip(SACI_SPRITE[6], True, False),
+                   ]
+MULA_SPRITE_INV = [pygame.transform.flip(MULA_SPRITE[0], True, False),
+                   pygame.transform.flip(MULA_SPRITE[1], True, False),
+                   pygame.transform.flip(MULA_SPRITE[2], True, False),
+                   pygame.transform.flip(MULA_SPRITE[3], True, False),
+                   pygame.transform.flip(MULA_SPRITE[4], True, False),
+                   pygame.transform.flip(MULA_SPRITE[5], True, False),
+                   pygame.transform.flip(MULA_SPRITE[6], True, False),
+                   ]
+
 IMAGE_SACI = pygame.transform.scale(pygame.image.load(os.path.join('img', 'saci.png')), (100, 100))
 IMAGE_MULA = pygame.transform.scale(pygame.image.load(os.path.join('img', 'mula.png')), (100, 100))
 IMAGE_BACKGROUND = pygame.transform.scale(pygame.image.load(os.path.join('img', 'bg.jpeg')), (800, 800))
@@ -48,6 +98,4 @@ IMAGE_BANNER_VIDA_OP = pygame.transform.scale(pygame.image.load(os.path.join('im
 
 #Fonte
 FONT = 'arial'
-FONT_TITLE = 'Helvetica'
-
-
+FONT_TITLE = 'arial'
--- main.py
+++ main.py
@@ -124,111 +124,226 @@ class Combat():
 
 class Game:
     def __init__(self):
+        #inicia o pygame
         pygame.init()
         pygame.display.set_icon(constants.LOGO)
         self.screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
         pygame.display.set_caption(constants.TITLE)
         self.clock = pygame.time.Clock()
-        self.font = pygame.font.SysFont("arial", 20)
+
+        #cria o loop do jogo
         self.running = True
+
+        #define a primeira tela para renderizar
         self.scrn = "Menu"
-        self.current_turn = "Player"
+
         self.winner = ""
+
+        #variaveis rolagem histõrico
         self.y = 20
         self.linVa = 50
         self.mx = 0
         self.n = 1
+
         self.op_ac = ""
+        self.canPlay = True
+
+        #definir a acao que o jogador fez
+        self.current_action = ""
+
+        #animacoes
+        self.va_c = 0;
+        self.cont_c = 0;
+        self.p_x = 70
 
 
     def run(self):
         print("rodando")
+        #loop principal do jogo
         while self.running:
-            self.clock.tick(constants.FPS)
+            self.clock.tick(constants.FPS) # 30FPS
             self.screen.blit(constants.IMAGE_BACKGROUND, (0,0))
-            if self.scrn == "Menu":
-                #self.screen.blit(constants.IMAGE_BACKGROUND, (0,0))
-                self.screen.blit(constants.IMAGE_TITLE, (270, 100))
-                if start_button.draw(self.screen, (constants.WIDTH/2) - 60, constants.HEIGHT/2 + 50):
-                    self.screen.fill(constants.BLACK)
-                    self.scrn = "Character"
-                elif history_button.draw(self.screen, (constants.WIDTH/2) - 60, constants.HEIGHT/2 + 150):
-                    self.linVa = 50
-                    self.scrn = "History"
-                elif info_button.draw(self.screen, constants.WIDTH - 60, constants.HEIGHT - 60):
-                    self.scrn = "Howtoplay"
 
+            #Menu Inicial
+            if self.scrn == "Menu":
+                self.screen.blit(constants.IMAGE_TITLE, (270, 100)) #renderiza o titulo //LEGENDS BRAZILIAN
+                #se start_button -> tela selecionar personagem
+                if start_button.draw(self.screen, (constants.WIDTH/2) - 60, constants.HEIGHT/2 + 50):
+                    self.scrn = "Character" #tela selecionar personagem
+                #se history_button -> tela histórico
+                elif history_button.draw(self.screen, (constants.WIDTH/2) - 60, constants.HEIGHT/2 + 150):
+                    self.linVa = 50 #define inicio da primeira linha
+                    self.scrn = "History" # tela histórico
+                #se info_button -> tela de como jogar
+                elif info_button.draw(self.screen, constants.WIDTH - 60, constants.HEIGHT - 60):
+                    self.scrn = "Howtoplay"#tela como jogar
+            
+            #Histórico de partidas
             elif self.scrn == "History":
                 self.screen.blit(constants.IMAGE_BACKGROUND_BLUR, (0,0))
-                self.read_data()
+                self.read_data() # lê o arquivo --./data/history.txt--, aonde os dados das partidas do jogador
                 self.draw_text("HISTÓRICO DE BATALHA", constants.FONT_TITLE, constants.BLACK, 300, 20)
+                #se back_button -> tela Menu
                 if back_button.draw(self.screen, constants.WIDTH - 150, constants.HEIGHT - 80):
                     self.scrn = "Menu"
 
+            #Como jogar
             elif self.scrn == "Howtoplay":
                 self.screen.blit(constants.IMAGE_BACKGROUND_BLUR, (0,0))
-                self.read_draw_howtoplay()
+                self.read_draw_howtoplay() #le o arquivo --./data/howtoplay.txt--
+                #se back_button -> tela Menu
                 if back_button.draw(self.screen, constants.WIDTH - 150, constants.HEIGHT - 80):
                     self.scrn = "Menu"
 
-            
+            #Pause -- so esta habilitado na tela de combate
             elif self.scrn == "Pause":
-                self.screen.blit(constants.IMAGE_BACKGROUND_BLUR, (0,0))
-                self.screen.blit(constants.IMAGE_TITLE, (400, constants.HEIGHT/2 - 35))
+                self.screen.blit(constants.IMAGE_BACKGROUND_BLUR, (0,0)) # fundo de tela
+                self.screen.blit(constants.IMAGE_TITLE, (400, constants.HEIGHT/2 - 35)) #renderiza o titulo //LEGENDS BRAZILIAN
+                #se continue_button -> retorna ao combate
                 if continue_button.draw(self.screen, constants.WIDTH/2 - 200, constants.HEIGHT/2 -100):
                     self.scrn = "Combat"
+                #se quit_button -> tela Menu
                 if quit_button.draw(self.screen, constants.WIDTH/2 - 200, constants.HEIGHT/2 + 50):
                     self.scrn = "Menu"
 
+            #Escolher personagem
             elif self.scrn == "Character":
-                self.screen.blit(constants.IMAGE_BACKGROUND_BLUR, (0,0))
+                self.screen.blit(constants.IMAGE_BACKGROUND_BLUR, (0,0)) # fundo de tela
                 self.draw_text("ESCOLHA SEU PERSONAGEM:", constants.FONT, constants.BLACK, 300, 30)
+                #se banner_saci -> seleciona o personagem saci para a batalha
                 if banner_saci.draw(self.screen, constants.WIDTH/2 - 325,  constants.HEIGHT/2 - 120):
                     self.choose_player(0)
                     self.scrn = "Combat"
+                #se banner_mula -> seleciona o personagem mula para a batalha
                 if banner_mula.draw(self.screen, constants.WIDTH/2 - 75, constants.HEIGHT/2 - 120):
                     self.choose_player(1)
                     self.scrn = "Combat"
+                #se banner_cuca -> seleciona o personagem cuca para a batalha
                 if banner_cuca.draw(self.screen, constants.WIDTH/2 + 175, constants.HEIGHT/2 - 120):
                     self.choose_player(2)
                     self.scrn = "Combat"
+                #se back_button -> tela Menu
                 if back_button.draw(self.screen, (constants.WIDTH/2) - 300, constants.HEIGHT - 100):
                     self.scrn = "Menu"
 
+                self.current_action = "stop"
+                self.canPlay = True
+
+                
+
+            #Tela combate
             elif self.scrn == "Combat":
                 self.screen.blit(constants.IMAGE_BACKGROUND_COMBAT, (0,0))
-                # Drawing the player's health
-                self.draw_player()
-                self.screen.blit(constants.IMAGE_BANNER_VIDA, (75, 200))
-                self.draw_text(str(self.ply.health), constants.FONT, constants.BLACK, 115, 205)
-                # Drawing the computer's health
+                self.draw_text("Esc - PAUSAR", constants.FONT, constants.WHITE, 10, 10)
+                #print(self.current_action)
+
+                # animacoes de cada acao
+                if self.current_action == "super_attack":
+                    self.attack_ani("super_attack", self.cont_c * 10)
+                    self.cont_c += 1
+                    if self.cont_c == 29:
+                        self.va_c = 0
+                        self.cont_c = 0
+                        self.canPlay = True
+                        self.current_action = "stop"
+                    self.draw_player()
+
+                elif self.current_action == "attack":
+                    self.attack_ani("attack", self.cont_c * 10)
+                    self.cont_c += 1
+                    if self.cont_c == 29:
+                        self.va_c = 0
+                        self.cont_c = 0
+                        self.canPlay = True
+                        self.current_action = "stop"
+                    self.draw_player()
+
+                elif self.current_action == "defend":
+                    self.cont_c += 1
+                    if self.cont_c == 29:
+                        self.va_c = 0
+                        self.cont_c = 0
+                        self.canPlay = True
+                        self.current_action = "stop"
+                    self.draw_player()
+
+                elif self.current_action == "jump":
+                    self.cont_c += 1
+                    if self.cont_c == 10:
+                        self.va_c = 0
+                        self.cont_c = 0
+                        self.canPlay = True
+                        self.current_action = "stop"
+                    self.draw_player()
+
+                else:
+                    self.va_c = 0
+                    self.draw_player()
+
+                # renderiza vida do player
+                self.screen.blit(constants.IMAGE_BANNER_VIDA, (75, 150))
+                self.draw_text(str(self.ply.health), constants.FONT, constants.BLACK, 115, 155)
+
+                # renderiza o computador e sua vida
                 self.draw_opponent()
-                self.screen.blit(constants.IMAGE_BANNER_VIDA_OP, (constants.WIDTH - 170, 200))
-                self.draw_text(str(self.opp.health), constants.FONT, constants.BLACK, constants.WIDTH - 150, 205)
+                self.screen.blit(constants.IMAGE_BANNER_VIDA_OP, (constants.WIDTH - 170, 150))
+                self.draw_text(str(self.opp.health), constants.FONT, constants.BLACK, constants.WIDTH - 150, 155)
 
-                self.draw_text(str(self.op_ac), constants.FONT, constants.WHITE, constants.WIDTH - 150, 140)
+                # renderiza a ultima acao do computador
+                self.draw_text(str(self.op_ac), constants.FONT, constants.WHITE, constants.WIDTH - 160, 130)
 
-                if atack_button.draw(self.screen, 100, constants.HEIGHT - 100):
-                    self.op_ac = self.com.action("attack")
-                elif super_atack.draw(self.screen, 250, constants.HEIGHT - 100):
-                    self.op_ac = self.com.action("super_attack")
-                elif defense_button.draw(self.screen, 450, constants.HEIGHT - 100):
-                    self.op_ac = self.com.action("defend")
-                elif jump_button.draw(self.screen, 600 , constants.HEIGHT - 100,):
-                    self.op_ac = self.com.action("jump")
+
+                if self.canPlay == True:
+                    #se atack_button -> jogador faz a acao de atacar
+                    if atack_button.draw(self.screen, 100, constants.HEIGHT - 100):
+                        self.va_c = 1
+                        self.current_action = "attack"
+                        self.cont_c = 0
+                        self.canPlay = False
+                        self.op_ac = self.com.action("attack")
+                    #se super_atack -> jogador faz a acao de super ataque
+                    if self.com.tim_sa_pl == 0:
+                        if (super_atack.draw(self.screen, 250, constants.HEIGHT - 100)):
+                            self.va_c = 2
+                            self.current_action = "super_attack"
+                            self.cont_c = 0
+                            self.canPlay = False
+                            self.op_ac = self.com.action("super_attack")
+                    else:
+                        constants.IMAGE_SUPER_ATTACK_BUTTON.set_alpha(60)
+                        self.screen.blit(constants.IMAGE_SUPER_ATTACK_BUTTON, (250, constants.HEIGHT - 100))
+                    #se defense_button -> jogador faz a acao de defesa
+                    if defense_button.draw(self.screen, 450, constants.HEIGHT - 100):
+                        self.va_c = 4
+                        self.current_action = "defend"
+                        self.cont_c = 0
+                        self.canPlay = False
+                        self.op_ac = self.com.action("defend")
+                    #se jump_button -> jogador faz a acao de pular
+                    if jump_button.draw(self.screen, 600 , constants.HEIGHT - 100):
+                        self.va_c = 3
+                        self.current_action = "jump"
+                        self.cont_c = 0
+                        self.canPlay = False
+                        self.op_ac = self.com.action("jump")
                 
-                self.draw_text(str(self.com.tim_sa_pl), constants.FONT, constants.BLACK, 300, constants.HEIGHT - 120)
+                #renderiza o numero de rodadas de espera para utilizar o super ataque novamente
+                #self.draw_text(str(self.com.tim_sa_pl), constants.FONT, constants.BLACK, 300, constants.HEIGHT - 120)
 
+                #verifica condicao termino do jogo
+                #se vida do jogador chegar em 0
                 if self.ply.health <= 0:
-                    self.scrn = "Result"
-                    self.winner = "Computer"
-                    self.write_data(str(self.ply.name), "DERROTA", str(self.opp.name))
-                    print("Player died")
+                    self.scrn = "Result" # -> Tela de Resultado
+                    self.winner = "Computer" # define o computador como vencedor
+                    self.write_data(str(self.ply.name), "DERROTA", str(self.opp.name)) # escreve no arquivo de dados o resultado
+                    # print("Player died")
+                #se vida do computador chegar em 0
                 elif self.opp.health <= 0:
-                    self.scrn = "Result"
-                    self.winner = "Player"
-                    self.write_data(str(self.ply.name), "VITORIA", str(self.opp.name))
-                    print("Player Win")
+                    self.scrn = "Result" # -> Tela de Resultado
+                    self.winner = "Player" # define o jogador como vencedor
+                    self.write_data(str(self.ply.name), "VITORIA", str(self.opp.name)) # escreve no arquivo de dados o resultado
+                    # print("Player Win")
+
 
             elif self.scrn == "Result":
                 self.screen.fill(constants.BLACK)
@@ -271,7 +386,8 @@ class Game:
         pygame.display.update()
     
     def draw_text(self, text, font, color, x, y):
-        img = self.font.render(text, True, color)
+        font = pygame.font.SysFont(font, 20)
+        img = font.render(text, True, color)
         self.screen.blit(img,( x, y))
 
     def show_message(self, message, duration):
@@ -293,21 +409,48 @@ class Game:
         self.opp = Computer(self.opponent.name, self.opponent.health, self.opponent.attack, self.opponent.defense)
         self.com = Combat()
     
+    def attack_ani(self, atck, x):
+        if atck == "attack":
+            if self.choose.name == "Mula sem cabeca":
+                power = constants.MULA_SPRITE[6]
+            elif self.choose.name == "Cuca":
+                power = constants.CUCA_SPRITE[5]
+            elif self.choose.name == "Saci":
+                power = constants.SACI_SPRITE[5]
+        else:
+            if self.choose.name == "Mula sem cabeca":
+                power = constants.MULA_SPRITE[5]
+            elif self.choose.name == "Cuca":
+                power = constants.CUCA_SPRITE[6]
+            elif self.choose.name == "Saci":
+                power = constants.SACI_SPRITE[6]
+
+    
+        self.screen.blit(power, (70 + x, 130))
+
     def draw_player(self):
         if self.choose.name == "Mula sem cabeca":
-            self.screen.blit(constants.IMAGE_MULA, (70, 250))
+            image = constants.MULA_SPRITE[self.va_c]
+            self.screen.blit(image, (30, 120))
         if self.choose.name == "Cuca":
-            self.screen.blit(constants.IMAGE_CUCA, (70, 250))
+            image = constants.CUCA_SPRITE[self.va_c]
+            self.screen.blit(image, (70, 150))
         if self.choose.name == "Saci":
-            self.screen.blit(constants.IMAGE_SACI, (70, 250))
+            image = constants.SACI_SPRITE[self.va_c]
+            self.screen.blit(image, (60, 130))
+
+        
 
     def draw_opponent(self):
-        if self.opponent.name == "Mula sem cabeca":
-            self.screen.blit(constants.IMAGE_MULA, (constants.WIDTH - 170, 250))
-        if self.opponent.name == "Cuca":
-            self.screen.blit(constants.IMAGE_CUCA, (constants.WIDTH - 170, 250))
-        if self.opponent.name == "Saci":
-            self.screen.blit(constants.IMAGE_SACI, (constants.WIDTH - 170, 250))
+        if self.opp.name == "Mula sem cabeca":
+            image = constants.MULA_SPRITE_INV[0]
+            self.screen.blit(image, (550, 120))
+        if self.opp.name == "Cuca":
+            image = constants.CUCA_SPRITE_INV[0]
+            self.screen.blit(image, (550, 150))
+        if self.opp.name == "Saci":
+            image = constants.SACI_SPRITE_INV[0]
+            self.screen.blit(image, (450, 130))
 
     def read_data(self):
         f = "data/history.txt"
